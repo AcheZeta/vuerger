d99ba05a102d0a3e785f5753f007b76dcab41a78
AcheZeta <ache.mariana@gmail.com>
2019-05-09 22:42:37 -0500

    nuevo proyecto

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <main>
+      <img src="./assets/burgerQueenLogo.png" alt="Vue.js PWA">
+       <br>
+      <router-view></router-view>
+      <input v-model="message" placeholder="¿Cómo te llamas?">
+      <button @click="add">¿Cuántas hamburguesas quieres?</button>
+      <p>Quieres {{ counter }} hamburguesas.</p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.counter += 1;
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
